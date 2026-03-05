681a2a8db3472f3fc25a4bdfc33c565e73f293d5
Hustler-dude <nickhustler12@gmail.com>
2026-03-05 01:02:48 +0000

    Add task 5: reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len, i;
+	char temp;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	len--;
+	i = 0;
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
+		len--;
+	}
+}
