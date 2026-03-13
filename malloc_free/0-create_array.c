1a9b118874aa569aeca32a0ea2f3d4eb4b2a3128
Hustler-dude <nickhustler12@gmail.com>
2026-03-13 22:24:52 +0000

    Add malloc free project and task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: char to initialize with
+ *
+ * Return: pointer to array, or NULL if fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,3 @@
+# C - malloc, free
+
+0-create_array.c - Create array of chars
