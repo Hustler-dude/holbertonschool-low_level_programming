29105484043fde416b9c02daeb5f0f72afed8e18
Hustler-dude <nickhuster12@gmail.com>
2026-04-13 21:08:10 +0200

    Add task 5: free_dog function

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog_t structure
+ * @d: pointer to the dog_t to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
