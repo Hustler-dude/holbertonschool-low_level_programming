2d4f43e7ccdc7a346cfef545a473f5ef711b78c4
Hustler-dude <nickhuster12@gmail.com>
2026-04-27 17:16:14 +0200

    Add recursion project: tasks 0-6 (puts, rev, strlen, factorial, pow, sqrt, prime)

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * prime_helper - helper to check primality recursively
+ * @n: the number to check
+ * @i: current divisor to test
+ *
+ * Return: 1 if no divisor found up to sqrt(n), 0 otherwise
+ */
+static int prime_helper(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (prime_helper(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if n is a prime number, 0 otherwise
+ * @n: the number to check
+ *
+ * Return: 1 if prime, 0 if not prime
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime_helper(n, 2));
+}
---
+++ recursion/README.md
@@ -0,0 +1,13 @@
+# C - Recursion
+
+Functions implemented using recursion (no loops, no static variables, no standard library).
+
+## Tasks
+
+- `0-puts_recursion.c` - prints a string followed by a newline
+- `1-print_rev_recursion.c` - prints a string in reverse
+- `2-strlen_recursion.c` - returns the length of a string
+- `3-factorial.c` - returns the factorial of a number
+- `4-pow_recursion.c` - returns x raised to the power of y
+- `5-sqrt_recursion.c` - returns the natural square root of a number
+- `6-is_prime_number.c` - returns 1 if the number is prime, 0 otherwise
